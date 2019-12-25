9e16e675db232e58a04410bbacd5d521700837d4
sonali mahavar <53106053+sonali-mahavar@users.noreply.github.com>
2019-12-25 11:05:20 +0530

    Add files via upload

---
+++ Do_whileLoop.java
@@ -0,0 +1,18 @@
+
+package do_while.loop;
+
+import java.util.Scanner;
+public class Do_whileLoop {
+
+    
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int i=1;
+       do{
+           System.out.println(i);
+           i++;
+           
+       }while(i<=10);
+    }
+    
+}
